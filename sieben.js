4b37ea56507ddaf2db0c5ffd63845416b91e0313
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-09 22:21:45 +0530

    Commit-VIII

--- sieben.js
+++ sieben.js
@@ -3,6 +3,28 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    const method = req.method;
+    if (url === '/form') {
+        res.write(`
+            <html>
+                <head>
+                    <title>
+                        Node Server
+                    </title>
+                </head>
+                <body>
+                    <form action="/redirector">
+                        <label for="message">Message:</label>
+                        <input type="text" id="message" placeholder="MESSAGE">
+                        <input type="submit" value="Submit">
+                    </form>
+                </body>
+            </html>
+        `);
+        return res.end();
+    }
+    if (url === "/redirector" && method === "POST")
     res.write(`
         <html>
             <head>
