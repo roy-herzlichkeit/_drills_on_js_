01d2b0074fc875e0a84f0a4f5a5cd10ea2fdceab
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-09 17:40:43 +0530

    Commit-IV

--- push.js
+++ push.js
@@ -1,6 +1,5 @@
 const { execSync } = require("child_process");
 const msg = process.argv[2] || "Update";
-execSync(`git add . && git commit -m "${msg}"`, {
+execSync(`git add . && git commit -m "${msg}" && git push -u origin main`, {
   stdio: "inherit",
 });
-// && git push -u origin main
---
+++ vier.js
@@ -0,0 +1,3 @@
+const objects = ['Pen', 'Paper', 'Charger', 'Laptop', 'Plant'];
+const [object1, object2] =  objects;
+console.log(object1, object2);
