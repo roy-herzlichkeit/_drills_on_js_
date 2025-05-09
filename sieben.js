688eaff58301df44f5977d25b6654012c25c999b
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-10 00:22:17 +0530

    Commit-XI

--- sieben.js
+++ sieben.js
@@ -1,50 +1,7 @@
 import http from "http";
-import fs from "fs";
+import routes from "./routes.js";
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`
-      <html>
-        <head><title>Enter Message</title></head>
-        <body>
-          <form action="/message" method="POST">
-            <label for="message">Message:</label>
-            <input type="text" id="message" name="message" placeholder="MESSAGE" />
-            <button type="submit">Submit</button>
-          </form>
-        </body>
-      </html>
-    `);
-    return res.end();
-  }
-
-  if (req.url === "/message" && req.method === "POST") {
-    const body = [];
-    req.on("data", chunk => {
-      console.log("chunk:", chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log("parsedBody:", parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message, "utf8");
-      console.log("message.txt written:", message);
-      res.writeHead(302, { Location: "/" });
-      return res.end();
-    });
-    return;
-  }
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(`
-    <html>
-      <head><title>Hello</title></head>
-      <body><h1>Hello World!</h1></body>
-    </html>
-  `);
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3301, () => {
   console.log("Server listening on http://localhost:3301");
