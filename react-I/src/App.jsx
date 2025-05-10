66d231c9e9e1f63e3bbd8e51bb7ba83ddc6a058e
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-10 15:51:12 +0530

    Commit-XIV

--- react-I/src/App.jsx
+++ react-I/src/App.jsx
@@ -1,13 +1,19 @@
+const textDesc = ['Fundamental', 'Crucial', 'Core', 'Rudimentary'];
+
+const getRandom = () => { 
+  return Math.floor(4 * Math.random());
+};
+
 const Header = () => {
   return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {textDesc[getRandom()]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
 }
 
