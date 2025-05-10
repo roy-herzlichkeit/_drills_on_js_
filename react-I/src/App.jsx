16a30575901d8fc147e26d35a976902f358a04c4
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-10 16:43:30 +0530

    Commit-XV

--- react-I/src/App.jsx
+++ react-I/src/App.jsx
@@ -1,3 +1,7 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+
 const textDesc = ['Fundamental', 'Crucial', 'Core', 'Rudimentary'];
 
 const getRandom = () => { 
@@ -5,15 +9,26 @@ const getRandom = () => {
 };
 
 const Header = () => {
+  const desc = textDesc[getRandom()];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {textDesc[getRandom()]} React concepts you will need for almost any app you are
+        {desc} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
+  );
+}
+
+const Component = ({image, title, description}) => {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
@@ -22,7 +37,17 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>
+            Core Concepts
+          </h2>
+          <ul>
+            <Component {...CORE_CONCEPTS[0]}/>
+            <Component {...CORE_CONCEPTS[1]}/>
+            <Component {...CORE_CONCEPTS[2]}/>
+            <Component {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
