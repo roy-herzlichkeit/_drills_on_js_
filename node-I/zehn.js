6b3897a76adf9ca3c403d5b43a93492f8a6cf2ef
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-13 00:06:07 +0530

    Commit-XXXII

---
+++ node-I/zehn.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded())
+
+app.use("/name", (req, res, next) => {
+    console.log(req.body.name);
+    res.redirect("/");
+});
+
+app.use("/add-name", (req, res, next) => {
+    res.send(`
+        <form action="/name" method="POST">
+            <input type="text" name="name">
+            <button type="submit">Submit</button>
+        </form>
+    `);
+});
+
+app.use("/", (req, res, next) => {
+    res.send("GO TO http://127.0.0.1:3000/add-name/");
+});
+
+app.listen(3000);
